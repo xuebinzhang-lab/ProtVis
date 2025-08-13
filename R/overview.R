e55a93f97768e57329cc396216986e081e923925
ZhangLabHenu <zhanglab_henu@163.com>
2025-08-13 23:08:22 +0800

    update overview

--- R/overview.R
+++ R/overview.R
@@ -378,7 +378,7 @@ overview_server <- function(id, shared_state) {
     plot_DR_results <- function(dr_data, sample_info, title_suffix) {
       df <- as.data.frame(dr_data) %>%
         dplyr::mutate(
-          SampleType = str_split(rownames(.), "_", 2, TRUE)[, 1],
+          SampleType = stringr::str_split(rownames(.), "_", 2, TRUE)[, 1],
           Type = str_remove_all(rownames(.), "^....|..$"),
           Species = case_when(
             SampleType == "B73" ~ "Zea mays ssp. mays",
