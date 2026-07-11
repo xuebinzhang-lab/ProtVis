1facad301404cab11155e096eceedbaaf4b64ec3
liangfei <103125590+anhuikylin@users.noreply.github.com>
2026-07-11 15:51:59 +0800

    Fix PTM wiring and data import validation

--- R/app_server.R
+++ R/app_server.R
@@ -33,6 +33,7 @@ app_server <- function(input, output, session) {
   enrichment_analysis_server("enrichment_analysis", shared_state = shared_state)
   gsea_server("gsea")
   pathview_server("pathview")
+  PTM_server("PTM")
   pd_strict_module_server("pd_strict")
   # protein_fun_server("protein_fun", shared_state)
   release_data_server("release_data1", shared_state)
