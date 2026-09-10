bcc479273a5486794e60f580e3eda5e8d46102e0
liangfei <103125590+anhuikylin@users.noreply.github.com>
2026-09-10 20:46:04 +0800

    Bundle real proteomics data (tests/testthat/test-builtin-sample-info.R)

--- tests/testthat/test-builtin-sample-info.R
+++ tests/testthat/test-builtin-sample-info.R
@@ -1,10 +1,12 @@
-test_that("compact built-in fixtures provide Control and Treatment metadata", {
+test_that("built-in fixtures provide real WT and oxidative-stress metadata", {
   for (source in c("DIA-NN", "FragPipe", "Skyline", "OpenMS")) {
     info <- .protvis_builtin_sample_info(source)
-    expect_equal(nrow(info), 4L)
+    expect_equal(nrow(info), 8L)
     expect_equal(info$maxquant_id,
-                 c("S1_Control", "S2_Control", "S3_Treatment", "S4_Treatment"))
-    expect_equal(info$group, c("Control", "Control", "Treatment", "Treatment"))
+                 c("WT1", "WT2", "WT3", "WT4", "WT_H2O2_1", "WT_H2O2_2",
+                   "WT_H2O2_3", "WT_H2O2_4"))
+    expect_equal(info$group, c(rep("WT", 4), rep("WT_H2O2", 4)))
     expect_equal(info$condition, info$group)
+    expect_true(all(info$organism == "Candida albicans"))
   }
 })
