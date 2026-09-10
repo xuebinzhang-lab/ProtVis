6cdc0b8c777847040eefbb3479eed8b8ac8d9c6f
liangfei <103125590+anhuikylin@users.noreply.github.com>
2026-09-10 17:56:31 +0800

    Validate metadata for all four built-in examples

--- tests/testthat/test-builtin-sample-info.R
+++ tests/testthat/test-builtin-sample-info.R
@@ -1,9 +1,10 @@
 test_that("compact built-in fixtures provide Control and Treatment metadata", {
-  info <- .protvis_builtin_sample_info("DIA-NN")
-
-  expect_equal(nrow(info), 4L)
-  expect_equal(info$maxquant_id,
-               c("S1_Control", "S2_Control", "S3_Treatment", "S4_Treatment"))
-  expect_equal(info$group, c("Control", "Control", "Treatment", "Treatment"))
-  expect_equal(info$condition, info$group)
+  for (source in c("DIA-NN", "FragPipe", "Skyline", "OpenMS")) {
+    info <- .protvis_builtin_sample_info(source)
+    expect_equal(nrow(info), 4L)
+    expect_equal(info$maxquant_id,
+                 c("S1_Control", "S2_Control", "S3_Treatment", "S4_Treatment"))
+    expect_equal(info$group, c("Control", "Control", "Treatment", "Treatment"))
+    expect_equal(info$condition, info$group)
+  }
 })
