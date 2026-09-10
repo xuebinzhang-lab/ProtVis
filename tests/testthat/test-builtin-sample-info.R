75a02591949a0c36b3a25ebfb49c0ef1e457f7f4
liangfei <103125590+anhuikylin@users.noreply.github.com>
2026-09-10 21:45:03 +0800

    Keep 5000 real proteins in compact built-in examples

--- tests/testthat/test-builtin-sample-info.R
+++ tests/testthat/test-builtin-sample-info.R
@@ -1,11 +1,9 @@
 test_that("built-in fixtures provide real B73 and Y12 metadata", {
   for (source in c("DIA-NN", "FragPipe", "Skyline", "OpenMS")) {
     info <- .protvis_builtin_sample_info(source)
-    expect_equal(nrow(info), 30L)
-    expect_equal(info$maxquant_id,
-                 c(paste0("B73_TMT", rep(1:3, each = 5), "_", rep(1:5, 3)),
-                   paste0("Y12_TMT", rep(1:3, each = 5), "_", rep(1:5, 3))))
-    expect_equal(info$group, c(rep("B73", 15), rep("Y12", 15)))
+    expect_equal(nrow(info), 2L)
+    expect_equal(info$maxquant_id, c("B73_TMT1_1", "Y12_TMT1_1"))
+    expect_equal(info$group, c("B73", "Y12"))
     expect_equal(info$condition, info$group)
     expect_true(all(info$organism == "Zea mays"))
     expect_true(all(grepl("ProtVis/blob/dev/inst/extdata/Maxquant_Export.xlsx",
